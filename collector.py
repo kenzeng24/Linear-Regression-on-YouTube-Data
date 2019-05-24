c81401733706f6d63eb0fcd717afca0518ffa30d
kenzeng24 <kgz1@rice.edu>
2019-05-24 12:00:48 -0500

    Tested some of the web scraper's features

--- collector.py
+++ collector.py
@@ -7,13 +7,8 @@
 
 import requests
 from lxml import html
-import csv
 from datetime import datetime
-from bs4 import BeautifulSoup
-from requests.exceptions import RequestException
-from contextlib import closing
 import csv
-from collections import defaultdict
 
 
 class Collector(object):
@@ -21,10 +16,11 @@ class Collector(object):
     A collector that takes a list of url as an input
     and scrapes data from those urls
     """
-    def __init__(self, url_list):
+    def __init__(self, id_list):
 
-        self.url_list = url_list
-        self.valid_urls = set([])
+        self.id_list = id_list
+        self.valid_ids = set([])
+        self.finished = False
 
         # records the date the views are collected
         self.date = datetime.now()
@@ -36,24 +32,41 @@ class Collector(object):
         # but what should I do with all the disabled channels?
         self.disabled = set([])
 
-    def get_views(self, url):
+        self.url_start = "https://www.youtube.com/watch?v="
+        self.url_list = [self.url_start + id for id in id_list]
+
+    def get_views(self, input, is_url=False):
         """
         return a dictionary mapping each url to
         all the information collected on the url
         """
-        if url in self.disabled:
-            return 0.0
-        return self.info[url]["views"]
+        if self.finished:
+            if is_url:
+                video_id = input[32:]
+            else:
+                video_id = input
+            if video_id in self.disabled:
+                return 0.0
+            return self.info[video_id]["views"]
+        else:
+            print("error: must run the web scraper first!")
 
-    def get_non_disabled(self):
+    def get_non_disabled(self, with_url=False):
         """
-        get a list of all YouTube videos that disabled views
+        with_url = true returns the list of urls
+        with_url = false returns the list of ids
+        get a list of all YouTube videos that did not disabled views
         """
-        non_disabled = set()
-        for url in self.valid_urls:
-            if url not in self.disabled:
-                non_disabled.add(url)
-        return non_disabled
+        if self.finished:
+            non_disabled = set()
+            for video_id in self.valid_ids:
+                if video_id not in self.disabled:
+                    non_disabled.add(video_id)
+            if with_url:
+                non_disabled = [self.url_start + video_id for video_id in non_disabled]
+            return non_disabled
+        else:
+            print("error: must run the web scraper first!")
 
     def run(self):
         """
@@ -62,72 +75,78 @@ class Collector(object):
         """
         print("start collection...")
 
-        for i in range(len(self.url_list)):
+        for i in range(len(self.id_list)):
             # sets a check point every time 500 websites are visited
             print(i)
             if i % 10 == 0 and i != 0:
                 print("visited  " + str(i) + " urls")
-            url = self.url_list[i]
+            video_id = self.id_list[i]
+            url = "https://www.youtube.com/watch?v=" + video_id
             # check if the url is a site on YouTube
-            if url[:29] != "https://www.youtube.com/watch":
+            if len(video_id) != 11:
                 pass
             try:
                 # try to access the webpage as an xpath
                 page = requests.get(url)
                 youtube = html.fromstring(page.text)
-                self.info[url] = {}
+                self.info[video_id] = {}
 
                 try:  # try to get title
                     title = youtube.xpath("//*[(@class='watch-title')]/text()")
-                    self.info[url]["title"] = str(title[0])
+                    self.info[video_id]["title"] = str(title[0]).split("\n")[1][4:]
                 except:
-                    self.disabled.append(url)
+                    self.disabled.append(video_id)
                 try:  # try to get date
                     title = youtube.xpath("//*[(@class='watch-time-text')]/text()")
-                    self.info[url]["date"] = str(title[0])
+                    # Todo: how do I convert a string date to numbers?
+                    self.info[video_id]["date"] = str(title[0])
                 except:
-                    self.disabled.append(url)
+                    self.disabled.append(video_id)
                 try:  # try to get views
                     view_count = youtube.xpath("//*[(@class='watch-view-count')]/text()")
-                    self.info[url]["views"] = float(str(view_count[0]).split(" ")[0].replace(',', ''))
+                    self.info[video_id]["views"] = float(str(view_count[0]).split(" ")[0].replace(',', ''))
                 except:
-                    self.disabled.append(url)
+                    self.disabled.append(video_id)
 
                 # Todo: get correct xpaths to likes and dislikes
                 # try:  # try to get likes
                 #     like_counts = youtube.xpath("//*[(@class='like-button-renderer-like-button')]/text()")
                 #     self.info[url]["likes"] = float(str(like_counts[0]).split(" ")[0].replace(',', ''))
                 # except:
-                #     disabled.append(url)
+                #     disabled.append(video_id)
                 # try:  # try to get dislikes
                 #     dislike_counts = youtube.xpath("//*[(@class='like-button-renderer-dislike-button')]/text()")
                 #     self.info[url]["dislikes"] = float(str(dislike_counts[0]).split(" ")[0].replace(',', ''))
                 # except:
-                #     disabled.append(url)
+                #     disabled.append(video_id)
 
-                self.valid_url.append(url)
+                self.valid_ids.add(video_id)
             except:
                 pass
 
+        self.finished = True
         print("Collection finished.")
 
     def save(self, file_name):
         """
-        save the file as a csv file
+        save contents of self.info as a csv file
         """
-        csv_file = open(file_name, 'w')
-        writer = csv.writer(csv_file)
-        row_titles = ["url", "title", "date", "views"]
-        writer.writerow(row_titles)
+        if self.finished:
+            csv_file = open(file_name, 'w')
+            writer = csv.writer(csv_file)
+            row_titles = ["video_id", "title", "date", "views"]
+            writer.writerow(row_titles)
 
-        # generate rest of the csv
-        for url in self.valid_urls:
-            if url not in self.disabled:
-                value = [url] + [self.info[url][name] for name in row_titles[1:]]
-                writer.writerow(value)
-        csv_file.close()
+            # generate rest of the csv
+            for video_id in self.valid_ids:
+                if video_id not in self.disabled:
+                    value = [video_id] + [self.info[video_id][name] for name in row_titles[1:]]
+                    writer.writerow(value)
+            csv_file.close()
+            print("File saved")
+        else:
+            print("error: must run the web scraper first!")
 
-        print("File saved")
 
 def stringtoint(stringdate):
     """
@@ -149,19 +168,19 @@ def num_days(year,month,day):
     monthdays = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
     return year * 365 + monthdays[month-1] + day
 
+
 if __name__ == "__main__":
 
     print("Hello World")
-    url_1 = "https://www.youtube.com/watch?v=87yupacw8Jc&t=3592s"
-    url_2 = "https://www.youtube.com/watch?v=1QCK_gFGi90"
-    url_list = [url_1, url_2]
-    web_scraper = Collector(url_list)
+    id_list = ["87yupacw8Jc", "1QCK_gFGi90", "ijn65GshaPs"]
+    web_scraper = Collector(id_list)
     web_scraper.run()
-    print(web_scraper.get_views(url_1))
-    print(web_scraper.get_views(url_2))
+    print(web_scraper.get_views(id_list[0]))
     # print(web_scraper.date.isocalendar())
     # datetime.fromisoformat()
-    # web_scraper.save("results.csv")
+    print(web_scraper.disabled)
+    print(web_scraper.valid_ids)
+    web_scraper.save("test.csv")
 
     # old_data = csv.reader(open("USvideos.csv"))
     # num_url = 100
--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-data = pd.read_csv("Results.csv")
 
 def plot(x,y,xlabel,ylabel):
     """
@@ -15,6 +14,7 @@ def plot(x,y,xlabel,ylabel):
     plt.ylabel(ylabel)
     plt.show()
 
+
 def log1p_plot(data,xlabel,ylabel):
     """
 
@@ -24,6 +24,7 @@ def log1p_plot(data,xlabel,ylabel):
     :return:
     """
     plot(np.log(data[xlabel] + 1), np.log(data[ylabel] + 1),xlabel, ylabel)
+
 
 def normal_plot(data,xlabel, ylabel):
     """
@@ -36,14 +37,17 @@ def normal_plot(data,xlabel, ylabel):
     plot(data[xlabel],data[ylabel], xlabel, ylabel)
 
 
-#data['x'] = data['likes'].apply(lambda x: remove_outliers(x,300000))
-#data['y'] = data['views_change'].apply(lambda x: remove_outliers(x, 5000000))
-#data['x'] = np.log(data['publish_time'] + 1)
-#data['y'] = np.log(data['views_change'] + 1)
-#data['%change'] = data['views_change'] / data['views']
+if __name__ == "__main__":
 
-#plot(data['publish_time'], data['y'],'publish_time','views_change')
-#plot(data['x'], data['y'],'comment_count','views_change')
+    data = pd.read_csv("Results.csv")
+    # data['x'] = data['likes'].apply(lambda x: remove_outliers(x,300000))
+    # data['y'] = data['views_change'].apply(lambda x: remove_outliers(x, 5000000))
+    # data['x'] = np.log(data['publish_time'] + 1)
+    # data['y'] = np.log(data['views_change'] + 1)
+    # data['%change'] = data['views_change'] / data['views']
 
-log1p_plot(data,'publish_time','views_change')
-normal_plot(data,'publish_time','views_change')
+    # plot(data['publish_time'], data['y'],'publish_time','views_change')
+    # plot(data['x'], data['y'],'comment_count','views_change')
+
+    log1p_plot(data, 'publish_time', 'views_change')
+    normal_plot(data, 'publish_time', 'views_change')
